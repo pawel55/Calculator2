cffa3bd41db76c4831702f9585f2b9629cb90d28
pawel55 <pawel.tokarek@wp.pl>
2017-05-25 00:07:25 +0200

    Graphic Calculator

---
+++ src/calculator2/Calculator2.java
@@ -0,0 +1,14 @@
+
+package calculator2;
+
+import javax.swing.JFrame;
+
+public class Calculator2 {
+
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // to close window
+        frame.setVisible(true); // to see window
+    }
+
+}
